e9273baf1d8a7c003dbfd8930ec75f87770041a2
Remilia Darknets <anarcho.dynabook@proton.me>
2025-10-04 17:53:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { noise } from "@chainsafe/libp2p-noise";
 
 const PROTOCOL = "/htp2p/0.0.1";
 
-async function startServer(filePath) {
+async function startServer() {
   const node = await createLibp2p({
     addresses: { listen: ["/ip4/0.0.0.0/tcp/0"] },
     transports: [tcp()],
