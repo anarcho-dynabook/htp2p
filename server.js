fc318255d6f9e4248c589bc524920ab3b416f83d
Remilia Darknets <anarcho.dynabook@proton.me>
2025-10-04 16:50:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import { createLibp2p } from "libp2p";
+import { lpStream } from "@libp2p/utils";
 import { tcp } from "@libp2p/tcp";
 import { mplex } from "@libp2p/mplex";
 import { noise } from "@chainsafe/libp2p-noise";
-import { readFileSync } from "node:fs";
 
 const PROTOCOL = "/htp2p/0.0.1";
 
-async function startServer() {
+async function startServer(filePath) {
   const node = await createLibp2p({
     addresses: { listen: ["/ip4/0.0.0.0/tcp/0"] },
     transports: [tcp()],
@@ -15,23 +15,14 @@ async function startServer() {
   });
 
   // Handle incoming file requests
-  node.handle(PROTOCOL, async ({ writeBuffer }) => {
-    console.log("[INFO] incoming request!");
-    let value = "Hello";
-    try {
-      writeBuffer = new TextEncoder().encode(value);
-      console.log("Response sent!");
-    } catch (err) {
-      console.error("Sink error:", err);
-    }
-    writeBuffer.close();
-    console.log("[INFO] Response sent!");
+  node.handle(PROTOCOL, async (stream) => {
+    const lp = lpStream(stream);
+    process.stdin.addListener("data", (buf) => lp.write(buf));
   });
 
   await node.start();
-  console.log("Server started with Peer ID:", node.peerId.toString());
-  console.log("Listening on addresses:");
-  node.getMultiaddrs().forEach((addr) => console.log(addr.toString()));
+  console.log("# Listening on addresses:");
+  node.getMultiaddrs().forEach((addr) => console.log(" -", addr.toString()));
 }
 
 startServer();
